2f5593f061561450db7819f95fa5f6d14c40ee06
Jon Martin <jomartin@udel.edu>
2022-07-18 10:47:21 -0700

    PhysWorld manages object map now

--- Runtime/Physics/PhysWorld.cs
+++ Runtime/Physics/PhysWorld.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEngine;
 using Unity.Mathematics.FixedPoint;
 using SepM.Utils;
@@ -14,6 +15,8 @@ namespace SepM.Physics {
         private List<Solver> m_solvers = new List<Solver>();
         // TODO: Serialize
         public List<PhysCollision> collisions = new List<PhysCollision>();
+        // TODO: How to seriealize?
+        public List<Tuple<GameObject, PhysObject>> objectsMap = new List<Tuple<GameObject, PhysObject>>();
 
         public void Serialize(BinaryWriter bw) {
         //m_objects
@@ -49,6 +52,19 @@ namespace SepM.Physics {
             AddSolver(new SmoothPositionSolver());
         }
 
+        public void AssignGameObject(GameObject g_o, PhysObject p_o) {
+            int existingIndex = objectsMap.FindIndex(t => t.Item2 == p_o);
+
+            // If not in the list, create a new value
+            if (existingIndex == -1) {
+                objectsMap.Add(new Tuple<GameObject, PhysObject>(g_o, p_o));
+            }
+            // If in the list, replace
+            else {
+                objectsMap[existingIndex] = new Tuple<GameObject, PhysObject>(g_o, p_o);
+            }
+        }
+
         public Tuple<GameObject, PhysObject> CreateSphereObject(fp3 center, fp r, bool isDyn, bool isKin, fp3 g) {
             return CreateSphereObject(center, r, isDyn, isKin, g, Constants.coll_layers.normal);
         }
@@ -78,6 +94,10 @@ namespace SepM.Physics {
 
             // Add to list of the world's physics objects
             m_objects.Add(p_obj);
+
+
+            // Add to the map
+            AssignGameObject(g_obj, p_obj);
 
             return result;
         }
@@ -182,7 +202,7 @@ namespace SepM.Physics {
         public void AddSolver(Solver solver) { m_solvers.Add(solver); }
         public void RemoveSolver(Solver solver) { /* TODO */ }
 
-        // TODO: Work with permissions
+        // Call in fixed timestep
         public void Step(fp dt) {
             ResolveCollisions(dt);
 
@@ -200,6 +220,16 @@ namespace SepM.Physics {
                 obj.Velocity = (newVelocity);
                 obj.Transform.Position = (newPosition);
                 obj.Force = (fp3.zero); // reset net force at the end
+            }
+        }
+
+        // Call with regular Unity Update()
+        public void UpdateGameObjects() {
+            // Update GameObjects
+            foreach (Tuple<GameObject, PhysObject> mapTuple in objectsMap) {
+                GameObject gameObject = mapTuple.Item1;
+                PhysObject physObject = mapTuple.Item2;
+                gameObject.transform.position = physObject.Transform.Position.toVector3();
             }
         }
 
