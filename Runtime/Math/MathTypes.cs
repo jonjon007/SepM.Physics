84322a8c3cef04101c0a72fdd2cb52dacbdd9f24
Jonathan Martin <jomartin@udel.edu>
2025-07-23 22:33:09 -0700

    Updating GetHashCode() for fpq

--- Runtime/Math/MathTypes.cs
+++ Runtime/Math/MathTypes.cs
@@ -16,7 +16,14 @@ namespace SepM.Math{
         public static fpq identity = new fpq(0,0,0,1);
         public static implicit operator Quaternion(fpq q)  { return new Quaternion((float)q.x, (float)q.y, (float)q.z, (float)q.w); }
         public static implicit operator fpq(Quaternion q)  { return new fpq((fp)q.x, (fp)q.y, (fp)q.z, (fp)q.w); }
-        public override int GetHashCode() { return (int)fpmath.hash(new fp4(x,y,z,w)); }
+        public override int GetHashCode() {
+            int hashCode = 1858597544;
+            hashCode = hashCode * -1521134295 + x.GetHashCode();
+            hashCode = hashCode * -1521134295 + y.GetHashCode();
+            hashCode = hashCode * -1521134295 + z.GetHashCode();
+            hashCode = hashCode * -1521134295 + w.GetHashCode();
+            return hashCode;
+        }
         public override string ToString(){
             return string.Format("fpq({0}, {1}, {2}, {3})", w, x, y, z);
         }
